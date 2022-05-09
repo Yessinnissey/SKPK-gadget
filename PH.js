45902ad56c6583970833c17023c3509d2ed860d3
Yessinnissey <105189469+Yessinnissey@users.noreply.github.com>
2022-05-09 11:57:23 +0700

    Upload pertama nih gaess

---
+++ PH.js
@@ -0,0 +1,132 @@
+let list = [];
+
+let gadget = {
+    foto: "https://asset.kompas.com/crops/kSiZzhgsoowsqTDCmIzJ82zd41o=/1x65:980x717/750x500/data/photo/2021/09/15/6140f06c43fea.jpg",
+    harga: 22000000,
+    kode: "BRG001",
+    nama: "iPhone 13 Pro Max",
+    rating: "5.0",
+    stock: 10
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://cf.shopee.co.id/file/1ccebe91143a5758c06fed900071e353",
+    harga: 50000000,
+    kode: "BRG004",
+    nama: "Samsung A7 Lite",
+    rating: "5.0",
+    stock: 20
+}
+list.push(gadget);
+
+gadget = {
+    foto: "https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-pro-5g-1-500x500.jpg",
+    harga: 7000000,
+    kode: "BRG005",
+    nama: "Oppo Reno 9",
+    rating: "5.0",
+    stock: 100
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://gadgetren.com/wp-content/uploads/2022/01/Xiaomi-Redmi-Note-11-Series-.jpg",
+    harga: 5000000,
+    kode: "BRG004",
+    nama: "Xiaomi Redmi Note 11",
+    rating: "5.0",
+    stock: 1000
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1637636360680/89c1d8a35799f58ad663f54b4cf97671.png",
+    harga: 6000000,
+    kode: "BRG005",
+    rating: "5.0",
+    stock: 50
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://static.bmdstatic.com/pk/richcontent/original/600003627324e.jpg",
+    harga: 2299000,
+    kode: " BRG006",
+    nama: "Samsung Galaxy A12",
+    rating: "5.0",
+    stock: 100
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://d1n6dbtoa2690v.cloudfront.net/article/5ce78bfcb92c2e07058b459a/5ce78bfcb92c2e07058b459a_1558679394.jpg",
+    kode: "BRG007",
+    nama: "Samsung Galaxy J2 Prime",
+    rating: "5.0",
+    stock: 20
+}
+
+list.push(gadget);
+
+
+gadget = {
+    foto: "https://awsimages.detik.net.id/community/media/visual/2019/02/25/51952a62-0f43-4379-8a5e-75d42b6a5275_43.jpeg?w=700&q=90",
+    harga: 3650000,
+    kode: "BRG008",
+    nama: "LG G8 ThinQ",
+    rating: "5.0",
+    stock: 50
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://asset.kompas.com/crops/lfhey5yznKbE9vTzXJ-tYE4aPHo=/173x0:949x517/750x500/data/photo/2021/09/10/613aa396c2053.jpg",
+    harga: 18500000,
+    kode: "BRG009",
+    nama: "Vivo x70 Pro+",
+    rating: "5.0",
+    stock: 22
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/2/16/59bec8dc-eb52-46be-95aa-8e76fca9c244.jpg",
+    harga: 2999000,
+    kode: "BRG010",
+    nama: "Realme 9 Pro",
+    rating: "5.0",
+    stock: 20
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://cf.shopee.co.id/file/65455492995cdde0ecbba5858a28e002",
+    harga: 10900000,
+    kode: "BRG011",
+    nama: "Vivo Y 11",
+    rating: "5.0",
+    stock: 35
+}
+
+list.push(gadget);
+
+gadget = {
+    foto: "https://www.harapanrakyat.com/wp-content/uploads/2021/08/HP-Realme-C11-2021.jpg",
+    harga: 1288000,
+    kode: "BRG012",
+    nama: "Realme C11",
+    rating: "5.0",
+    stock: 30
+}
+
+list.push(gadget);
+console.log(list);
